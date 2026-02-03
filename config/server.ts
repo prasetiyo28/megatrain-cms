7c518c5eeee5dd67608208064e6fefce93fe3624
Bayu Adi Prasetiyo <bayu28.bap@gmail.com>
2026-02-03 20:14:30 +0700

    change config

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,13 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
+  url: env('PUBLIC_URL', 'https://api.megatrain.id'),
+
+  admin: {
+    url: env('ADMIN_URL', 'https://admin.megatrain.id'),
+  },
+
   app: {
     keys: env.array('APP_KEYS'),
   },
